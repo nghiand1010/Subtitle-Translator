cef1b3099b2326c6d1e2450a09cf078946ccbd4d
nghiand1010 <nghiand1010@gmail.com>
2024-02-20 23:37:23 +0700

    ADD TRANSLATE

--- scripts/content.js
+++ scripts/content.js
@@ -5,7 +5,8 @@ var target = document.querySelector('.ytp-caption-window-container')
 
 // Create an observer instance.
 var observer = new MutationObserver(function(mutations) {
-    console.log(target.innerText);   
+   // console.log(target.innerText);  
+    translate(target.innerText);
 });
 
 // Pass in the target node, as well as the observer options.
@@ -14,3 +15,24 @@ observer.observe(target, {
     childList:     true,
     characterData: true
 });
+
+async function translate(sourceText){
+  
+    var sourceLang = 'en';
+    var targetLang = 'vi';
+   
+
+    if (!sourceText)
+        return;
+    
+    console.log(sourceText);
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+
+    const response = await fetch(url);
+    const result = await response.json();
+    var tempTranslate = result[0];
+    for (let index = 0; index < tempTranslate?.length; index++) {
+        const element = tempTranslate[index];
+        console.log(element[0]);
+    }
+}
