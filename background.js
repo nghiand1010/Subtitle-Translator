df3fcca388af49ef98141315baa2deaa301298b6
nghiand1010 <nghiand1010@gmail.com>
2024-03-06 05:43:03 +0700

    Add storages

--- background.js
+++ background.js
@@ -1,52 +1,79 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
+
+var _status=false;
+function getDomain(url, subdomain) {
+  subdomain = subdomain || true;
+  if(!url)
+      return url;
+
+  url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+  if (!subdomain) {
+      url = url.split('.');
+
+      url = url.slice(url.length - 2).join('.');
+  }
+
+  if (url.indexOf('/') !== -1) {
+      return url.split('/')[0];
+  }
+
+  return url;
+}
+
+  chrome.tabs.onActivated.addListener( function(activeInfo){
+    setBadgeTextByStatus();
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+  setBadgeTextByStatus();
+});
+
+function setBadgeTextByStatus()
+{
+  chrome.tabs.query({  
+  "active": true,
+  "currentWindow": true,
+  "status": "complete",
+  "windowType": "normal"}, async function(tabs)
+    {
+      for(t in tabs)
+      {
+        let tab=tabs[t];
+        var chkkey=getDomain(tab.url);
+        chrome.storage.sync.get(chkkey).then(async (result) => {
+          let status=false;
+          if(result[chkkey])
+          {
+            let storage=JSON.parse(result[chkkey]);
+            status=storage.status;
+            _status=status;
+          }
+          const nextState = status === true ? 'OFF' : 'ON';
+          await chrome.action.setBadgeText({
+            tabId:tabs[t].id,
+            text: nextState
+          });
+        })
+    } });
+  
+}
+
+chrome.storage.onChanged.addListener((changes, namespace) =>
+{
+  setBadgeTextByStatus();
+ });
+
+ chrome.runtime.onMessage.addListener( async function(request, sender, sendResponse) {
+
+  if (request.method == "getStatus")
+  {
+    sendResponse(_status);
+  }
+  else
+    sendResponse({}); // snub them.
+});
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: 'OFF'
-    });
-  });
-  
-  const extensions = 'https://developer.chrome.com/docs/extensions';
-  const webstore = 'https://developer.chrome.com/docs/webstore';
-  
-  // When the user clicks on the extension action
-  chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-      // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-      // Next state will always be the opposite
-      const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  
-      // Set the action badge to the next state
-      await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState
-      });
-  
-      if (nextState === 'ON') {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-          files: ['focus-mode.css'],
-          target: { tabId: tab.id }
-        });
-      } else if (nextState === 'OFF') {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-          files: ['focus-mode.css'],
-          target: { tabId: tab.id }
-        });
-      }
-    }
-  });
+  setBadgeTextByStatus();
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,92 @@
-console.log("This is a popup!")
+// popup.js
+// Get the elements from the popup
+let lblEnable = document.getElementById('lblEnable');
+let chkEnable = document.getElementById('enable');
+let cmbLangFrom = document.getElementById('lang-from');
+let cmbLangTo= document.getElementById('lang-to');
+let domain;
+
+// Define a function to handle the change event
+function handleChange(event) {
+    let chkkey=`${domain}`;
+    var key = chkkey,
+    testPrefs = JSON.stringify(
+      { status: !event.target.checked}
+    );
+    var jsonfile = {};
+    jsonfile[key] = testPrefs;
+
+    chrome.storage.sync.set(jsonfile).then(() => {
+        console.log("Value is set");
+      });
+    //.setItem(`${domain}_disable`,JSON.stringify(!event.target.checked));
+  }
+
+  // Add the event listener to the checkbox
+  chkEnable.addEventListener("change", handleChange);
+
+// Load the options from the local storage
+function loadOptions() {
+
+chrome.tabs.query({ //This method output active URL 
+    "active": true,
+    "currentWindow": true,
+    "status": "complete",
+    "windowType": "normal"
+}, function (tabs) {
+    for (tab in tabs) {
+        domain=getDomain(tabs[tab].url,true);
+        lblEnable.innerHTML=`Enable on <b>${domain}<b/>`;
+        initControl();
+    }
+});
+
+}
+
+function initControl()
+{
+     // localStorage.setItem("lastname", "Smith");
+     let chkkey=`${domain}`;
+     console.log('init');
+     chrome.storage.sync.get(chkkey).then((result) => {
+      // console.log("Value is " + result[chkkey]);
+      let disable=false;
+      if(result[chkkey])
+      {
+        let value=JSON.parse( result[chkkey]);
+        disable=value.status;
+      }
+   
+       setCheckboxEnableValue(!disable);
+     });
+     
+}
+
+function setCheckboxEnableValue(value) {
+    chkEnable.checked=value;
+    console.log('checkbox',chkEnable.checked);
+}
+
+function getDomain(url, subdomain) {
+    subdomain = subdomain || true;
+    if(!url)
+        return url;
+
+    url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+    if (!subdomain) {
+        url = url.split('.');
+
+        url = url.slice(url.length - 2).join('.');
+    }
+
+    if (url.indexOf('/') !== -1) {
+        return url.split('/')[0];
+    }
+
+    return url;
+}
+
+// Call the loadOptions function when the popup is loaded
+document.addEventListener("DOMContentLoaded", loadOptions);
+//document.addEventListener("Loaded", loadOptions);
